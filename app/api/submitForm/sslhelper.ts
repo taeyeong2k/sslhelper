d3a384473e926d3f478c10bebe2bf62b3ea34ae5
Tae's PC <53635845+taeyeong2k@users.noreply.github.com>
2023-10-23 12:21:10 +1100

    fix check domain

--- app/api/submitForm/sslhelper.ts
+++ app/api/submitForm/sslhelper.ts
@@ -8,7 +8,7 @@ const execAsync = promisify(exec);
 
 const fetchSslInfo = async (domain: string) => {
   try {
-    const command = `echo | openssl s_client -connect ${domain}:443 2>/dev/null | openssl x509 -noout  -subject -issuer -email -dates -ext "subjectAltName"`;
+    const command = `echo | openssl s_client -connect ${domain}:443 2>/dev/null | openssl x509 -noout -text -certopt no_header -certopt ca_default`;
     const { stdout, stderr } = await execAsync(command);
     
     if (stderr) {
