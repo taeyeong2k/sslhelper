b164739e6102543e625e542c483633584db09826
Tae Yeong Kim <53635845+taeyeong2k@users.noreply.github.com>
2023-12-15 11:33:05 +1100

    formatting

--- app/api/submitForm/sslhelper.ts
+++ app/api/submitForm/sslhelper.ts
@@ -1,16 +1,16 @@
-const { exec } = require('child_process');
-const { promisify } = require('util');
-import os from 'os';
-import path from 'path';
-import fs from 'fs';
+const { exec } = require("child_process");
+const { promisify } = require("util");
+import os from "os";
+import path from "path";
+import fs from "fs";
 const execAsync = promisify(exec);
-const { formatOutput } = require('../../utils/formatHelper.ts');
+const { formatOutput } = require("../../utils/formatHelper.ts");
 
 const fetchSslInfo = async (domain: string) => {
   try {
     const command = `echo | openssl s_client -connect ${domain}:443 -servername ${domain} 2>/dev/null | openssl x509 -noout -text`;
     const { stdout, stderr } = await execAsync(command);
-    
+
     if (stderr) {
       console.error(`An error occurred: ${stderr}`);
       return;
@@ -27,8 +27,8 @@ const fetchSslInfo = async (domain: string) => {
     // prepend ip to stdout
     const ip = await execAsync(ipCommand);
     const output = `${domain} resolves to ${ip.stdout}${formattedOutput}`;
-    console.log('IP Address: ' + ip.stdout);
-    console.log('SSL Certificate Information:\n');
+    console.log("IP Address: " + ip.stdout);
+    console.log("SSL Certificate Information:\n");
     return output;
   } catch (e) {
     console.error(`An exception occurred: ${e}`);
@@ -38,17 +38,17 @@ const fetchSslInfo = async (domain: string) => {
 export const csrDecode = async (csr: string) => {
   // Create a temporary file in the system's temp directory
   const tempFilePath = path.join(os.tmpdir(), `temp_csr_${Date.now()}.pem`);
-  
+
   // Write the CSR to the temporary file
   fs.writeFileSync(tempFilePath, csr);
-  
+
   // Run the OpenSSL command
   const command = `openssl req -in ${tempFilePath} -noout -text`;
   const { stdout, stderr } = await execAsync(command);
 
   // Remove the temporary file
   fs.unlinkSync(tempFilePath);
-  
+
   if (stderr) {
     console.error(`An error occurred: ${stderr}`);
     return;
@@ -58,33 +58,33 @@ export const csrDecode = async (csr: string) => {
 };
 
 export const checkDomain = async (domain: string) => {
-    console.log("Checking domain " + domain)
-    const result = await fetchSslInfo(domain);
-    console.log(result)
-    return result;
-  };
-  
+  console.log("Checking domain " + domain);
+  const result = await fetchSslInfo(domain);
+  console.log(result);
+  return result;
+};
+
 export const decodeSslCertificate = async (certificateContent: string) => {
   try {
-  // Create a temporary file in the system's temp directory
-  const tempFilePath = path.join(os.tmpdir(), `temp_crt_${Date.now()}.crt`);
-  
-  // Write the CSR to the temporary file
-  fs.writeFileSync(tempFilePath, certificateContent);
-  
+    // Create a temporary file in the system's temp directory
+    const tempFilePath = path.join(os.tmpdir(), `temp_crt_${Date.now()}.crt`);
+
+    // Write the CSR to the temporary file
+    fs.writeFileSync(tempFilePath, certificateContent);
+
     // OpenSSL command to decode the certificate
     const command = `openssl x509 -in ${tempFilePath} -text -noout`;
-    console.log("command: " + command)
+    console.log("command: " + command);
     // Execute the command
     const { stdout, stderr } = await execAsync(command);
-  // Remove the temporary file
-  fs.unlinkSync(tempFilePath);
+    // Remove the temporary file
+    fs.unlinkSync(tempFilePath);
     if (stderr) {
       console.error(`An error occurred: ${stderr}`);
       return;
     }
 
-    console.log('Decoded SSL Certificate Information:\n');
+    console.log("Decoded SSL Certificate Information:\n");
     console.log(stdout);
     const formattedOutput = formatOutput(stdout);
     return formattedOutput;
@@ -93,59 +93,71 @@ export const decodeSslCertificate = async (certificateContent: string) => {
   }
 };
 
-
-export const certificateKeyMatcher = async (certificate: string, key: string, csr: string) => {
+export const certificateKeyMatcher = async (
+  certificate: string,
+  key: string,
+  csr: string
+) => {
   try {
     var checkCsr = false;
     var checkCert = false;
     // check if csr is empty
-    if (csr === '') {
+    if (csr === "") {
       console.log("CSR is empty");
     } else {
       checkCsr = true;
     }
 
     // check if cert is empty
-    if (certificate === '') {
+    if (certificate === "") {
       console.log("Certificate is empty");
     } else {
       checkCert = true;
     }
-    console.log("checkCsr: " + checkCsr)
-    console.log("checkCert: " + checkCert)
+    console.log("checkCsr: " + checkCsr);
+    console.log("checkCert: " + checkCert);
     // Create temporary file for the key
-    const tempKeyFilePath = path.join(os.tmpdir(), `temp_key_${Date.now()}.pem`);
+    const tempKeyFilePath = path.join(
+      os.tmpdir(),
+      `temp_key_${Date.now()}.pem`
+    );
     fs.writeFileSync(tempKeyFilePath, key);
     // Create temporary file for CSR, if needed
-    let tempCsrFilePath = '';
+    let tempCsrFilePath = "";
     if (checkCsr) {
       tempCsrFilePath = path.join(os.tmpdir(), `temp_csr_${Date.now()}.pem`);
       fs.writeFileSync(tempCsrFilePath, csr);
     }
 
     // Create temporary file for the certificate if needed
-    let tempCertFilePath = '';
+    let tempCertFilePath = "";
     if (checkCert) {
       tempCertFilePath = path.join(os.tmpdir(), `temp_cert_${Date.now()}.pem`);
       fs.writeFileSync(tempCertFilePath, certificate);
     }
 
     // Extract the modulus and exponent from the certificate if needed
-    let modulusCert = '';
+    let modulusCert = "";
     if (checkCert) {
-      const { stdout: stdoutCert } = await execAsync(`openssl x509 -pubkey -noout -in ${tempCertFilePath} | openssl rsa -pubin -modulus -noout`);
-      modulusCert = stdoutCert.replace('Modulus=', '').trim();
+      const { stdout: stdoutCert } = await execAsync(
+        `openssl x509 -pubkey -noout -in ${tempCertFilePath} | openssl rsa -pubin -modulus -noout`
+      );
+      modulusCert = stdoutCert.replace("Modulus=", "").trim();
     }
 
     // Extract the modulus and exponent from the key
-    const { stdout: stdoutKey } = await execAsync(`openssl rsa -modulus -noout -in ${tempKeyFilePath}`);
-    const modulusKey = stdoutKey.replace('Modulus=', '').trim();
+    const { stdout: stdoutKey } = await execAsync(
+      `openssl rsa -modulus -noout -in ${tempKeyFilePath}`
+    );
+    const modulusKey = stdoutKey.replace("Modulus=", "").trim();
 
     // Extract the modulus from the CSR, if needed
-    let modulusCsr = '';
+    let modulusCsr = "";
     if (checkCsr) {
-      const { stdout: stdoutCsr } = await execAsync(`openssl req -in ${tempCsrFilePath} -noout -modulus`);
-      modulusCsr = stdoutCsr.replace('Modulus=', '').trim();
+      const { stdout: stdoutCsr } = await execAsync(
+        `openssl req -in ${tempCsrFilePath} -noout -modulus`
+      );
+      modulusCsr = stdoutCsr.replace("Modulus=", "").trim();
     }
 
     // Remove the temporary files
@@ -156,49 +168,51 @@ export const certificateKeyMatcher = async (certificate: string, key: string, cs
       fs.unlinkSync(tempCsrFilePath);
     }
     if (checkCert) {
-      fs.unlinkSync(tempCertFilePath)
+      fs.unlinkSync(tempCertFilePath);
     }
 
     // Perform matching logic
     // Check cert and key only
-  if (checkCert && !checkCsr) {
-    if (modulusCert === modulusKey) {
-      return 'Certificate and key matched!';
-    } else {
-      return 'Certificate and key did NOT match!';
+    if (checkCert && !checkCsr) {
+      if (modulusCert === modulusKey) {
+        return "Certificate and key matched!";
+      } else {
+        return "Certificate and key did NOT match!";
+      }
     }
-  }
 
-  // Check cert, key, and CSR
-  if (checkCert && checkCsr) {
-    if (modulusCert === modulusKey && modulusCert === modulusCsr) {
-      return 'Certificate, key, and CSR all matched!';
-    } else if (modulusCert === modulusKey) {
-      return 'Certificate and key matched, but CSR did NOT match!';
-    } else {
-      return 'Certificate and key did NOT match!';
+    // Check cert, key, and CSR
+    if (checkCert && checkCsr) {
+      if (modulusCert === modulusKey && modulusCert === modulusCsr) {
+        return "Certificate, key, and CSR all matched!";
+      } else if (modulusCert === modulusKey) {
+        return "Certificate and key matched, but CSR did NOT match!";
+      } else {
+        return "Certificate and key did NOT match!";
+      }
     }
-  }
 
-  // Check CSR and key only
-  if (!checkCert && checkCsr) {
-    if (modulusKey === modulusCsr) {
-      return 'CSR and key matched!';
-    } else {
-      return 'CSR and key did NOT match!';
+    // Check CSR and key only
+    if (!checkCert && checkCsr) {
+      if (modulusKey === modulusCsr) {
+        return "CSR and key matched!";
+      } else {
+        return "CSR and key did NOT match!";
+      }
     }
-  }
-
   } catch (e) {
     console.error(`An exception occurred: ${e}`);
-    return 'Error';
+    return "Error";
   }
 };
 
 export const verifyCertificateChain = async (certificateChain: string) => {
   try {
     // Create a temporary file to store the certificate chain
-    const tempChainFilePath = path.join(os.tmpdir(), `temp_chain_${Date.now()}.pem`);
+    const tempChainFilePath = path.join(
+      os.tmpdir(),
+      `temp_chain_${Date.now()}.pem`
+    );
     fs.writeFileSync(tempChainFilePath, certificateChain);
 
     // OpenSSL command to verify the certificate chain
@@ -212,20 +226,19 @@ export const verifyCertificateChain = async (certificateChain: string) => {
 
     if (stderr) {
       console.error(`An error occurred: ${stderr}`);
-      return 'Verification failed';
+      return "Verification failed";
     }
 
     // If the output includes 'OK', the verification was successful
-    if (stdout.includes('OK')) {
-      console.log('Certificate chain verified successfully');
-      return 'Verification successful';
+    if (stdout.includes("OK")) {
+      console.log("Certificate chain verified successfully");
+      return "Verification successful";
     } else {
-      console.log('Certificate chain verification failed');
-      return 'Verification failed';
+      console.log("Certificate chain verification failed");
+      return "Verification failed";
     }
-
   } catch (e) {
     console.error(`An exception occurred: ${e}`);
-    return 'Error';
+    return "Error";
   }
 };
