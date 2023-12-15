3471333a4e500b8d98b334cfbeea6a2da30875b7
Tae Yeong Kim <53635845+taeyeong2k@users.noreply.github.com>
2023-12-15 11:26:01 +1100

    Format with prettier

--- app/utils/formatHelper.ts
+++ app/utils/formatHelper.ts
@@ -3,38 +3,43 @@ const countryPattern = /Subject:.*?C\s*=\s*([^\n\r,]+)/;
 const statePattern = /Subject:.*?ST\s*=\s*([^\n\r,]+)/;
 const locationPattern = /Subject:.*?L\s*=\s*([^\n\r,]+)/;
 const organizationPattern = /Subject:.*?O\s*=\s*(?:")?([^\n\r",]+)(?:"|,|$)/;
-const organizationUnitPattern = /Subject:.*?OU\s*=\s*(?:")?([^\n\r",]+)(?:"|,|$)/;
+const organizationUnitPattern =
+  /Subject:.*?OU\s*=\s*(?:")?([^\n\r",]+)(?:"|,|$)/;
 const commonNamePattern = /Subject:.*?CN\s*=\s*([^\n\r,]+)/;
-const altNamesPattern = /X509v3 Subject Alternative Name:\s*((?:DNS:[^\n\r,]+,?\s*)+)/;
+const altNamesPattern =
+  /X509v3 Subject Alternative Name:\s*((?:DNS:[^\n\r,]+,?\s*)+)/;
 const validFromPattern = /Not Before:\s*([^\n\r]+)/;
 const validToPattern = /Not After :\s*([^\n\r]+)/;
 const issuerCNPattern = /Issuer:.*?CN\s*=\s*([^,\n]+)/;
 const emailAddressPattern = /Subject:.*?emailAddress\s*=\s*([^\n\r,]+)/;
 
 const findValue = (pattern: RegExp, output: string): string => {
-    const match = output.match(pattern);
-    return match ? match[1].trim() : '';
+  const match = output.match(pattern);
+  return match ? match[1].trim() : "";
 };
 
 export function formatOutput(output: string): string {
-    // Helper function to find a value using a regex pattern
+  // Helper function to find a value using a regex pattern
 
-    // Extract information using defined patterns
-    const country = findValue(countryPattern, output);
-    const state = findValue(statePattern, output);
-    const location = findValue(locationPattern, output);
-    const organization = findValue(organizationPattern, output);
-    const organizationUnit = findValue(organizationUnitPattern, output); // Modify this accordingly
-    const commonName = findValue(commonNamePattern, output);
-    let altNames = findValue(altNamesPattern, output);
-    altNames = altNames.replace(/DNS:/g, '').replace(/\s*,\s*/g, ', ').trim();
-    const validFrom = findValue(validFromPattern, output);
-    const validTo = findValue(validToPattern, output);
-    const issuerCN = findValue(issuerCNPattern, output);
-    const emailAddress = findValue(emailAddressPattern, output);
+  // Extract information using defined patterns
+  const country = findValue(countryPattern, output);
+  const state = findValue(statePattern, output);
+  const location = findValue(locationPattern, output);
+  const organization = findValue(organizationPattern, output);
+  const organizationUnit = findValue(organizationUnitPattern, output); // Modify this accordingly
+  const commonName = findValue(commonNamePattern, output);
+  let altNames = findValue(altNamesPattern, output);
+  altNames = altNames
+    .replace(/DNS:/g, "")
+    .replace(/\s*,\s*/g, ", ")
+    .trim();
+  const validFrom = findValue(validFromPattern, output);
+  const validTo = findValue(validToPattern, output);
+  const issuerCN = findValue(issuerCNPattern, output);
+  const emailAddress = findValue(emailAddressPattern, output);
 
-    // Format the extracted information
-    const formattedOutput = `
+  // Format the extracted information
+  const formattedOutput = `
 =============================================
              CERT decoded text
 =============================================
@@ -50,7 +55,6 @@ Valid To            : ${validTo}
 Email Address       : ${emailAddress}
 Issuer              : ${issuerCN}
 =============================================
-`
-    return formattedOutput;
+`;
+  return formattedOutput;
 }
-
