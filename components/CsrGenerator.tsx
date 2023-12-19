c78daf19b3c74d41f73a89c4db964a869ca6e9b3
Tae Yeong Kim <53635845+taeyeong2k@users.noreply.github.com>
2023-12-19 14:39:52 +1100

    remove form from component, use main submit button

--- components/CsrGenerator.tsx
+++ components/CsrGenerator.tsx
@@ -1,56 +1,83 @@
 // CSRGenerator.tsx
 
-import React, { useState } from 'react';
-import { Flex, TextArea} from "@radix-ui/themes";
+import React, { useState } from "react";
+import { Flex, TextArea } from "@radix-ui/themes";
 
 const CSRGenerator = () => {
-  const [commonName, setCommonName] = useState('');
-  const [organization, setOrganization] = useState('');
-  const [organizationalUnit, setOrganizationalUnit] = useState('');
-  const [country, setCountry] = useState('');
-  const [state, setState] = useState('');
-  const [email, setEmail] = useState('');
-  const [location, setLocation] = useState('');
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log('Generating CSR with:', { commonName, organization, organizationalUnit, country, state, email });
-    // CSR generation logic goes here
-  };
+  const [commonName, setCommonName] = useState("");
+  const [organization, setOrganization] = useState("");
+  const [organizationalUnit, setOrganizationalUnit] = useState("");
+  const [country, setCountry] = useState("");
+  const [state, setState] = useState("");
+  const [email, setEmail] = useState("");
+  const [location, setLocation] = useState("");
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Flex direction="column" gap="3">
-        <label>
-          Common Name (CN):
-          <TextArea value={commonName} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setCommonName(e.target.value)} />
-        </label>
-        <label>
-          Organization (O):
-          <TextArea value={organization} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setOrganization(e.target.value)} />
-        </label>
-        <label>
-          Organizational Unit (OU) (Optional):
-          <TextArea value={organizationalUnit} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setOrganizationalUnit(e.target.value)} />
-        </label>
-        <label>
-          Country (2 letter code) (C):
-          <TextArea value={country} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setCountry(e.target.value)} />
-        </label>
-        <label>
-          State or Province Name spelled out (ST):
-          <TextArea value={state} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setState(e.target.value)} />
-        </label>
-        <label>
-            Location - City name spelled out (L):
-            <TextArea value={location} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setLocation(e.target.value)} />
-        </label>
-        <label>
-          Email:
-          <TextArea value={email} onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setEmail(e.target.value)} />
-        </label>
-      </Flex>
-    </form>
+    <Flex direction="column" gap="3">
+      <label>
+        Common Name (CN):
+        <TextArea
+          value={commonName}
+          onChange={(e: { target: { value: React.SetStateAction<string> } }) =>
+            setCommonName(e.target.value)
+          }
+        />
+      </label>
+      <label>
+        Organization (O):
+        <TextArea
+          value={organization}
+          onChange={(e: { target: { value: React.SetStateAction<string> } }) =>
+            setOrganization(e.target.value)
+          }
+        />
+      </label>
+      <label>
+        Organizational Unit (OU) (Optional):
+        <TextArea
+          value={organizationalUnit}
+          onChange={(e: { target: { value: React.SetStateAction<string> } }) =>
+            setOrganizationalUnit(e.target.value)
+          }
+        />
+      </label>
+      <label>
+        Country (2 letter code) (C):
+        <TextArea
+          value={country}
+          onChange={(e: { target: { value: React.SetStateAction<string> } }) =>
+            setCountry(e.target.value)
+          }
+        />
+      </label>
+      <label>
+        State or Province Name spelled out (ST):
+        <TextArea
+          value={state}
+          onChange={(e: { target: { value: React.SetStateAction<string> } }) =>
+            setState(e.target.value)
+          }
+        />
+      </label>
+      <label>
+        Location - City name spelled out (L):
+        <TextArea
+          value={location}
+          onChange={(e: { target: { value: React.SetStateAction<string> } }) =>
+            setLocation(e.target.value)
+          }
+        />
+      </label>
+      <label>
+        Email:
+        <TextArea
+          value={email}
+          onChange={(e: { target: { value: React.SetStateAction<string> } }) =>
+            setEmail(e.target.value)
+          }
+        />
+      </label>
+    </Flex>
   );
 };
 
