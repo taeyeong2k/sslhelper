be3aafddde4a2fa9b79702f2f008cebe6ada84ab
Tae Yeong Kim <53635845+taeyeong2k@users.noreply.github.com>
2023-12-19 11:47:54 +1100

    add textarea component file

---
+++ components/TextAreaInput.tsx
@@ -0,0 +1,26 @@
+// TextAreaInput.tsx
+
+import React from 'react';
+import { TextArea as RadixTextArea } from "@radix-ui/themes";
+
+interface TextAreaInputProps {
+  selectedButton: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  placeholder: string;
+}
+
+const TextAreaInput: React.FC<TextAreaInputProps> = ({ selectedButton, value, onChange, placeholder }) => {
+  const textAreaHeight = selectedButton !== "Check Domain" && selectedButton !== "Select an option" ? "h-[250px]" : "h-[50px]";
+
+  return (
+    <RadixTextArea
+      className={`w-full ${textAreaHeight}`}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export default TextAreaInput;
