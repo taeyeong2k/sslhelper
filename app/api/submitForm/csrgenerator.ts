84e63b89895abdd02f1cefe9f7b8421dc5b31d05
Tae Yeong Kim <53635845+taeyeong2k@users.noreply.github.com>
2024-01-03 13:45:52 +1100

    fix imports

--- app/api/submitForm/csrgenerator.ts
+++ app/api/submitForm/csrgenerator.ts
@@ -1,6 +1,10 @@
 const { exec } = require("child_process");
 const { promisify } = require("util");
 import { CsrData } from "@/app/utils/types";
+import os from "os";
+import path from "path";
+import fs from "fs";
+const execAsync = promisify(exec);
 export function generateCsr(input: CsrData) {
   return "Test";
 }
